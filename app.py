912ed6129f8cc61ce95c8e2cc47930b89b7480c7
xtify <dave.battaglia@gmail.com>
2017-12-08 16:56:30 -0500

    updated app with dyanamic routes, and statucodes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 #flask app test
 
-#import the flask class from the flask package
+#import the flask class from the flask  q
 
 from flask import Flask
 
@@ -16,9 +16,21 @@ app = Flask(__name__)
 @app.route("/hello")
 
 #define the view using a function, which returns a string
-
 def hello_world():
-    return "hello world"
+    return "hello, world!?!?!?!"
+
+#dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+#dynamic route w/explicit status codes
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "david" :
+        return "hello, {}".format(name)
+    else:
+        return "not found", 404
 
 #start the dev server using the run() methods
 
